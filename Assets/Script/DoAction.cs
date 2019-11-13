61582b7a3dce171bff9c8ef82aac4f2b3f7383f7
aurore <Pauwelsa@live.be>
2019-11-13 12:18:31 +0100

    Travail du Matin du 13.11.19 principalement Asset mais debut de code

---
+++ Assets/Script/DoAction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoAction: MonoBehaviour
+{
+    public bool needItem = false;
+    [Header("si avoir besoin Item est Vrai")]
+    public string itemType;
+    public string itemID;// let null insector if not necessary
+
+}
