65d7dde083e380dbd3396165769c40f62b85f487
aurore <Pauwelsa@live.be>
2019-12-17 10:12:48 +0100

    !!!!!!

---
+++ Assets/Script/QuetesManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class QuetesManager : MonoBehaviour
+{
+    //public static QuetesManager questManager;
+
+    //public List<QuetesManager> quetesList = new List<QuetesManager>();
+
+    //public List<QuetesManager> currentQuetesList = new List<QuetesManager>();
+
+    ////private vars for les objets de quetes.
+
+    //private void Awake()
+    //{
+    //    if(questManager == null)
+    //    {
+    //        questManager = this;
+    //    }
+    //    else if(questManager!= this)
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //    DontDestroyOnLoad(gameObject);
+    //}
+
+    //// bool 
+
+    //public bool RequestAvailableQuest(int questID)
+    //{
+    //    for (int i = 0; i < quetesList.Count; i++)
+    //    {
+    //        if ()
+    //    }
+    //}
+}
